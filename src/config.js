8ecb828b3b9d06576926f4a5562a6970df9eeffe
YOOKIKI <ny90208@naver.com>
2022-03-15 22:21:14 +0900

    Modify: 불필요한 코드 및 라이브러리 삭제, 이미지 css 수정

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'http://10.58.5.138:8000';
+
+export const API = {
+  KakaoLogin: `${BASE_URL}/users/kakao-auth`,
+};
+
+export default API;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,39 @@
+import React from 'react';
 import './Login.scss';
 
-export default function Login() {
-  return <h1>Login</h1>;
-}
+const Login = () => {
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
+
+  return (
+    <div className="login">
+      <div className="loginBox">
+        <div className="loginBoxSub">
+          <div className="flexbox">
+            <div className="leftBox">
+              <div className="leftBoxDetail">
+                <div className="leftLogoBox">
+                  <h1 className="islogin">CLNASS505</h1>
+                </div>
+                <div className="kakaoLogin">
+                  <div className="loginTextBox">
+                    <p>3초만에 카카오로 로그인하기!</p>
+                    <div>
+                      <a href={KAKAO_AUTH_URL} className="KakaoBtn">
+                        <img src="/images/kakao_login3.png" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="rightBox">
+              <img src="/images/yoojung2.jpg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

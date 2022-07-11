5a4cb48082a8348d0d00f7ab9603f4e201119900
YOOKIKI <ny90208@naver.com>
2022-07-11 23:21:04 +0900

    fix config

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main/Main';
 import List from './pages/List/List';
 import Login from './pages/Login/Login';
 import KakaoLogin from './pages/Login/KakaoLogin';
@@ -12,7 +11,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />;
+        <Route path="/" element={<List />} />;
         <Route path="/list" element={<List />} />;
         <Route path="/login" com element={<Login />} />;
         <Route path="/oauth/kakao" element={<KakaoLogin />} />
--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-const BASE_URL = 'http://172.20.10.3:8000';
+const BASE_URL = 'http://3.38.14.143';
 
 export const API = {
   login: `${BASE_URL}/users/login`,
   lectures: `${BASE_URL}/lectures`,
-  likes: `${BASE_URL}`,
+  likes: `${BASE_URL}/lectures`,
   KakaoLogin: `${BASE_URL}/users/kakao-auth`,
 };
 
